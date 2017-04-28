427c2d8869c960d1af832d200b43d965c417a228
GreeceArtemis <greece.rapu@gmail.com>
2017-04-28 12:39:39 -0500

    contraseñas validas

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,31 @@
-var enviar=document.getElementById("enviar");
-var validacionNombre=/^([A-Z]{1}[a-z]+[\s]*)+$/;
-enviar.addEventListener("click",function(e){
-  e.preventDefault();
-  //[0-9]{8}
-  var nombre = document.getElementById("nombre").value;
-  if( validacionNombre.test(nombre) ) {
-    alert("error");
+
+/*
+/^(\([0-9]{2,3}\))+([0-9]{2,3})+[ |-]+([0-9]{4})/
+/\d{8}/g
+^/[9]{1}\d{8}/
+/^([A-ZÁÉÍÓÚ]{1}[a-zñáéíóú]*[\s]?)+$/
+*/
+
+window.onload = function(){
+    var formulario = document.getElementById('formulario');
+    var boton_enviar = document.getElementById('enviar');
+    var nota1=document.getElementById("nota1");
+    nota1.style.display="none";
+      boton_enviar.addEventListener("click",function(e){
+        var contrasena=document.getElementById("contrasena");
+        var confirmaContrasena=document.getElementById("confirmaContrasena");
+        contrasena=contrasena.value;
+        confirmaContrasena=confirmaContrasena.value;
+          if(contrasena!==confirmaContrasena)
+              {
+              e.preventDefault();
+              nota1.style.display="block";
+              }
+              else{
+                nota1.style.display="none";
+              }
+      });
+    if(formulario.checkValidity()){
+          alert('El formulario es correcto!');
+        }
   }
-});
